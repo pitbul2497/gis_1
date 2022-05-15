68edc87231cab6436d9d32de89f0864d1f48fd05
Borzhikov Temirlan <pitshow277@gmail.com>
2022-05-15 18:30:26 +0700

    up

---
+++ theme/treeNodes.js
@@ -0,0 +1,4 @@
+theApp.nav.treeViews = [
+{id: "view:_jrOeENQkEey45akVU3diGg", objectId: "_jrOeENQkEey45akVU3diGg", label: "Процесс работы", src: "theme/work_process_55848DB5.json", isDefault: true},
+{id: "view:_Cmrq0NQ_Eey45akVU3diGg", objectId: "_Cmrq0NQ_Eey45akVU3diGg", label: "ГИС", src: "theme/gis_9104B069.json", isDefault: false}];
+
